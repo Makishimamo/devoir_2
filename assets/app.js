1dd6a779643091ce84cc14aa8ed8342da76b3e62
Maximin Pony <150128278+Makishimamo@users.noreply.github.com>
2024-12-05 09:10:18 +0100

    check fields & showMessageError

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,21 @@
 // Test : appel du fichier
 //alert("Hello World");
+
+// Création de la fonction pour faire apparaitre le message d'erreur
+function showErrorMessage() {
+    document.getElementById('error-message').style.display = 'block';
+}
+
+// Selectionner les champs du formulaire
+let prenom = document.getElementById('first-name');
+let nom = document.getElementById('last-name');
+let message = document.getElementById('message');
+
+// Vérifier chaque champs, si vide lancer la fonction showErrorMessage()
+if (prenom.value === '') {
+    showErrorMessage();
+}   else if (nom.value === '') {
+    showErrorMessage();
+}   else if (message.value === '') {
+    showErrorMessage();
+}
