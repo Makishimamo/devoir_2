bac657c40f83d23271dde7b0cf3b7964ec36aed5
Maximin Pony <150128278+Makishimamo@users.noreply.github.com>
2024-12-06 19:28:55 +0100

    add function addCom

--- assets/app.js
+++ assets/app.js
@@ -26,12 +26,39 @@ function champVide() {
     });
 }
 
-// Céation d'un fonction qui va ajouter le commentaire dans la div id="comment-list"
-function commenter() {
-    alert ("Bouton cliquer") ;
+// Création d'une fonction pour créer le nouveau commentaire
+const commentList =  document.getElementById('comment-list');
+const nomV = document.getElementById('last-name').value;
+const prenomV = document.getElementById('first-name').value;
+const messageV = document.getElementById('message').value;
+const myForm = document.getElementById('form');
+
+function addCom(nomV, prenomV, messageV) {
+    
+    // Crée une <div>
+    const newCom = document.createElement('div');
+    
+    // Ajoute la class à la <div> créer
+    newCom.classList.add('flex', 'space-x-4', 'text-sm', 'text-gray-500'); 
+    
+    // J'insère le code suivant dans ma <div>
+    newCom.innerHTML = ` 
+        <div class="flex-1 py-10 border-t border-gray-200"> 
+            <h3 class="font-medium text-gray-900">${nomV} ${prenomV}</h3> 
+            <div class="prose prose-sm mt-4 max-w-none text-gray-500"> 
+                <p>${messageV}</p> 
+            </div> 
+        </div> 
+    `;
+
+    // Ajoute le commentaire à la suite des autres
+    commentList.appendChild(newCom);
+
+    // Efface les valeurs contenue dans le formulaire
+    myForm.reset(); 
 }
 
 // Lier la fonction au bouton "Envoyer"
 const myButton = document.querySelector('button');
 myButton.addEventListener('click', champVide);
-myButton.addEventListener('click', commenter);
+myButton.addEventListener('click', addCom);
